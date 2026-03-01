e96d0602503a74ce3a87d1459178e20dc591266b
PokipokiBagre <amanojaku344@gmail.com>
2026-03-01 12:24:18 -0500

    Update obj-data.js

--- objetos/obj-data.js
+++ objetos/obj-data.js
@@ -1,5 +1,5 @@
 import { invGlobal, objGlobal, guardar } from './obj-state.js';
-const normalizar = (str) => str ? str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().trim() : "";
+
 export async function cargarTodoDesdeCSV() {
     const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQDaZ1Zr9YWmgW05Hzpv4IQzpMaKrgSvVUm_Yrps3DdwwPpIjD4iHrdLyPHGucuTHnwwYdM7bPrcnRO/pub?output=csv&cachebust=" + new Date().getTime();
     try {
@@ -19,5 +19,5 @@ export async function cargarTodoDesdeCSV() {
             });
         });
         guardar();
-    } catch (e) { console.error(e); }
+    } catch (e) { console.error("Error cargando CSV:", e); }
 }
