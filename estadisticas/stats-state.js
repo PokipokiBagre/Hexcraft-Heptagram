fbcd8b2840e7d12adc002d2b9a5740a4d7ee6aec
PokipokiBagre <amanojaku344@gmail.com>
2026-03-02 18:30:01 -0500

    Update stats-state.js

--- estadisticas/stats-state.js
+++ estadisticas/stats-state.js
@@ -3,9 +3,9 @@ export let estadoUI = {
     personajeActivo: null,
     esAdmin: false,
     paginaActiva: 'publico',
-    principales: [] 
+    principales: [] // Dueños provenientes de Objetos
 };
 
 export function guardar() { 
-    localStorage.setItem('hex_stats_vFinal_v3', JSON.stringify(statsGlobal)); 
+    localStorage.setItem('hex_stats_vPersistence_Final', JSON.stringify(statsGlobal)); 
 }
