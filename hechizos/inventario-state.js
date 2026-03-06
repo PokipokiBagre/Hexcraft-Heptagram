edff88d01a1c6550f5ddf6b979b89161ff30912c
PokipokiBagre <amanojaku344@gmail.com>
2026-03-06 00:52:43 -0500

    Update inventario-state.js

--- hechizos/inventario-state.js
+++ hechizos/inventario-state.js
@@ -1,22 +1,13 @@
 export let db = {
-    personajes: {}, // Formato: { Nombre: { isActive, isPlayer, hex, cantHechizos, mayorAfinidad, icono, rawRow } }
-    csvHeadersPersonajes: [], // Guardamos la cabecera para poder exportar el CSV
-    hechizos: {
-        nodos: [],
-        nodosOcultos: [],
-        inventario: [],
-        string: []
-    }
+    personajes: {}, csvHeadersPersonajes: [],
+    hechizos: { nodos: [], nodosOcultos: [], inventario: [], string: [] }
 };
 
 export let estadoUI = {
-    vistaActual: 'catalogo', 
-    personajeSeleccionado: null,
-    esAdmin: false,
-    filtroRol: 'Todos',
-    filtroAct: 'Todos',
-    filtrosGrimorio: { afinidad: 'Todos', busqueda: '' }, // Filtros dentro del inventario de un Pj
-    filtrosGestion: { afinidad: 'Todos', busqueda: '' },
-    restarHexAsignacion: true, // Switch para restar HEX al asignar en OP
+    vistaActual: 'catalogo', personajeSeleccionado: null, esAdmin: false,
+    filtroRol: 'Todos', filtroAct: 'Todos',
+    filtrosGrimorio: { afinidad: 'Todos', busqueda: '' },
+    filtrosGestion: { afinidad: 'Todos', clase: 'Todos', busqueda: '' },
+    restarHexAsignacion: true,
     colaCambios: { agregar: [], quitar: [] }
 };
