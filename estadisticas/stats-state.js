d940ab11925af327d2a11ab818e7ae9becf3179d
PokipokiBagre <amanojaku344@gmail.com>
2026-03-07 15:25:24 -0500

    Update stats-state.js

--- estadisticas/stats-state.js
+++ estadisticas/stats-state.js
@@ -6,11 +6,11 @@ export let estadoUI = {
     esAdmin: false,
     filtroRol: 'Todos',
     filtroAct: 'Todos',
-    party: [null, null, null, null, null, null], 
-    hexLog: {}
+    party: [null, null, null, null, null, null], // Slots
+    hexLog: {} // Memoria del Log Agrupado
 };
 
-// Contenedor para las otras bases de datos
+// Contenedor para las otras bases de datos cruzadas
 export let dbExtra = {
     objetos: {}, // Guarda { "linda": 15, "corvin": 8 }
     hechizos: { inventario: [], nodos: [], nodosOcultos: [] }
